74c39a34ac8d7912897cfc463a84b092b8b92c9c
zoyadkostova <96051484+zoyadkostova@users.noreply.github.com>
2022-04-10 14:33:03 +0300

    Update task7.java

--- src/com/operatorshomework/task7.java
+++ src/com/operatorshomework/task7.java
@@ -10,10 +10,9 @@ public class task7 {
         int list1[] = {10, 20, 30, 40, 50};
         int arrayLength = list1.length - 1;
 
-        // print list in Reverse using for loop
         for (int i = arrayLength; i >= 0; i--)
         {
-            // access elements by their index (position)
+            
             System.out.print(list1[i] + " ");
         }
     }
