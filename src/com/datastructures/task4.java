dbd44eb2d05e4ed826454a4a8e5c94328a4c2aa2
AD\zoya.kostova <zkostova@augeomarketing.com>
2022-04-17 16:49:45 +0300

    datastructures and methods homeworks added

--- src/com/datastructures/task4.java
+++ src/com/datastructures/task4.java
@@ -1,25 +1,32 @@
 package com.datastructures;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Scanner;
+
 public class task4 {
 
-    // Java Program to find maximum in arr[]
     public static void main(String[] args) {
         {
-             int arr[] = {10, 2, 3, 4, 5};
+            ArrayList<Integer> list = new ArrayList<Integer>();
 
-                int i;
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Input list length: ");
+            int length = sc.nextInt();
 
-                // Initialize maximum element
-                int max = arr[0];
-
-                // Traverse array elements from second and
-                // compare every element with current max
-                for (i = 1; i < arr.length; i++){
-                    if (arr[i] > max)
-                        max = arr[i];
+            System.out.print("Add first number in the list: ");
+            int i=0;
+            for (i = 0; i < length; i++) {
+                System.out.print("Add next number: ");
+                int number = sc.nextInt();
+                list.add(number);
             }
-            System.out.println("Largest in given array is " + max);
+
+            System.out.println("Before Sorting: " + list);
+            Collections.sort(list);
+            System.out.println("After Sorting: " + list);
+            System.out.println("Biggest number in the list: " + list.get(i-1));
         }
     }
-
 }
